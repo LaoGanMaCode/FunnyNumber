f8e642b0c0dd7e4844c076d1664d2afcc74c2bc3
LaoGanMaCode <kanin.a@kkumail.com>
2022-10-12 17:07:28 +0700

    testing with divide 3

--- lab13.java
+++ lab13.java
@@ -1,10 +1,24 @@
+
+import java.util.Scanner;
 public class lab13 {
-	
-	
-	public int oneisTure() {
-		int input = 1;
-		return input;
+
+
+	public String isNumber() {
+		Scanner input = new Scanner(System.in);
+		String result;
+
+		float num = input.nextFloat();
+		float sult = num/3;
+
+		if(sult == 1) {
+			result = "Fizz";
+			return result;
+		}else {
+			String s=String.valueOf(sult);
+			return s;
+		}
+
+
 	}
-	
 	
 }
